c55add64946cbeadb3b9a1daf33232ef404bbb37
brianna404 <briannakruschke@gmx.de>
2024-08-11 17:55:51 +0200

    [WIP][Bookmarking]-Prevent reloading of image

--- IOSApp/Views/ArticleView.swift
+++ IOSApp/Views/ArticleView.swift
@@ -24,57 +24,67 @@ struct ArticleView: View {
     var body: some View {
         // vertical stack to display article details
         HStack {
-            // URLImage used for image loading
-            if let imgUrl = article.urlToImage,
-               let url = URL(string: imgUrl) {
-                URLImage(url) {
-                    // This view is displayed before download starts
-                    EmptyView()
-                } inProgress: { progress in
-                    // Display progress
-                    Text("Loading...")
-                } failure: { error, retry in
-                    //if error accured
-                    PlaceholderImageView()
-                } content: { image in
-                    // Downloaded image
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                }
+            // load image
+            ArticleImageView(imgUrl: article.urlToImage)
                 
-                VStack(alignment: .leading, spacing: 4) {
-                    Text(article.author ?? "")
-                        .foregroundStyle(Color.gray)
-                        .font(.footnote)
-                    HStack {
-                        Text(article.title ?? "")
-                            .foregroundStyle(Color.black)
-                            .font(.system(size: 18, weight: .semibold))
-                        Spacer()
-                        Button(action: {
-                        // Call the toggleBookmark function from ArticleStorage
-                        let updatedArticle = articleStorage.toggleBookmark(for: article)
-                        isBookmarked = updatedArticle.isBookmarked ?? false
-                        }) {
-                            Image(systemName: isBookmarked ? "bookmark.fill" : "bookmark")
-                        }                    }
-                    Text(DateUtils.formatDate(dateString: article.publishedAt ?? ""))
-                        .foregroundStyle(Color.gray)
-                        .font(.system(size: 11))
-                }
+            // display information
+            VStack(alignment: .leading, spacing: 4) {
+                Text(article.author ?? "")
+                    .foregroundStyle(Color.gray)
+                    .font(.footnote)
+                HStack {
+                    Text(article.title ?? "")
+                        .foregroundStyle(Color.black)
+                        .font(.system(size: 18, weight: .semibold))
+                    Spacer()
+                    Button(action: {
+                    // Call the toggleBookmark function from ArticleStorage
+                    let updatedArticle = articleStorage.toggleBookmark(for: article)
+                    isBookmarked = updatedArticle.isBookmarked ?? false
+                    }) {
+                        Image(systemName: isBookmarked ? "bookmark.fill" : "bookmark")
+                    }                    }
+                Text(DateUtils.formatDate(dateString: article.publishedAt ?? ""))
+                    .foregroundStyle(Color.gray)
+                    .font(.system(size: 11))
             }
         }
     }
+}
     
-    // placeholder view for the article image if image loading fails
-    struct PlaceholderImageView: View {
-        var body: some View {
-            Image(systemName: "photo.fill") // Placeholder image
-                .foregroundStyle(Color.white)
-                .background(Color.gray) // Background color for placeholder
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/) // size
+// image loading logic -> separated to prevent image from reloading when bookmark icon is clicked
+struct ArticleImageView: View {
+    let imgUrl: String?
+    
+    var body: some View {
+        if let imgUrl = imgUrl, let url = URL(string: imgUrl) {
+            URLImage(url) {
+                // view displayed before download starts
+                EmptyView()
+            } inProgress: { progress in
+                // Display progress
+                Text("Loading...")
+            } failure: { error, retry in
+                // if error accured
+                PlaceholderImageView()
+            } content: { image in
+                // Downloaded image
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100)
+            }
         }
     }
 }
+
+// placeholder view for the article image if image loading fails
+struct PlaceholderImageView: View {
+    var body: some View {
+        Image(systemName: "photo.fill") // Placeholder image
+            .foregroundStyle(Color.white)
+            .background(Color.gray) // Background color for placeholder
+            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/) // size
+    }
+}
+
